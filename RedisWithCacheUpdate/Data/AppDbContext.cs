59527f30ae5d9b8c439aa7b3007e58bc6376d418
Bekzod Allaev <Allaev_@outlook.com>
2025-04-14 21:18:29 +0200

    Add db context and models

---
+++ RedisWithCacheUpdate/Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using System;
+using Microsoft.EntityFrameworkCore;
+using RedisWithCacheUpdate.Model;
+
+namespace RedisWithCacheUpdate.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Optionally, override OnModelCreating to specify more configuration.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Category>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)        // A Product has one Category
+                .WithMany(c => c.Products)      // A Category has many Products
+                .HasForeignKey(p => p.CategoryId)  // Specify the foreign key property
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ RedisWithCacheUpdate/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace RedisWithCacheUpdate.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
