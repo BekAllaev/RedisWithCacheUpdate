f1bc3d137dc936bed23eb65bfef0774684872e16
Bekzod Allaev <Allaev_@outlook.com>
2025-04-19 20:28:26 +0200

    Add StatisticController

---
+++ RedisWithCacheUpdate/Controllers/StatisticController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisWithCacheUpdate.StatisticsModel;
+
+namespace RedisWithCacheUpdate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticController : ControllerBase
+    {
+        public StatisticController() { }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductsByCategory>>> GetProductsByCategories()
+        {
+
+
+            return Ok();
+        }
+    }
+}
