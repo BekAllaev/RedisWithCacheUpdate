b59d15f2100e3fd65a87abeed931cf41ea4bf4c4
Bekzod Allaev <Allaev_@outlook.com>
2025-04-19 20:27:33 +0200

    Add statistics model - ProductsByCategory

---
+++ RedisWithCacheUpdate/StatisticsModel/ProductsByCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace RedisWithCacheUpdate.StatisticsModel
+{
+    /// <summary>
+    /// Amount of product in each category
+    /// </summary>
+    public class ProductsByCategory
+    {
+        public string CategoryName { get; set; }
+
+        public int ProductCount { get; set; }
+    }
+}
