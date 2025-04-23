1c33b1b6f19d5465802c36f30b3780c876645ae4
Bekzod Allaev <Allaev_@outlook.com>
2025-04-23 15:04:48 +0200

    Implement GET method

--- RedisWithCacheUpdate/Controllers/StatisticController.cs
+++ RedisWithCacheUpdate/Controllers/StatisticController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using RedisWithCacheUpdate.Extensions;
+using RedisWithCacheUpdate.Services;
 using RedisWithCacheUpdate.StatisticsModel;
 
 namespace RedisWithCacheUpdate.Controllers
@@ -8,14 +11,19 @@ namespace RedisWithCacheUpdate.Controllers
     [ApiController]
     public class StatisticController : ControllerBase
     {
-        public StatisticController() { }
+        private readonly IDistributedCache distributedCache;
+
+        public StatisticController(IDistributedCache distributedCache)
+        { 
+            this.distributedCache = distributedCache;
+        }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ProductsByCategory>>> GetProductsByCategories()
         {
+            var list = await distributedCache.GetAsync<List<ProductsByCategory>>(Constants.PRODUCTS_BY_CATEGORIES_REDIS_KEY);
 
-
-            return Ok();
+            return list;
         }
     }
 }
